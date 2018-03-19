83d489d4f155559b047e932cf42a0c2ba31c28f6
YOLO <yanglu@thejoyrun.com>
2018-03-19 11:15:06 +0800

    ignore config

--- config/default.js
+++ config/default.js
@@ -6,8 +6,6 @@ module.exports = {
     DATABASE: 'blog',
     PORT: '3306'
   },
-  // GITHUB_ID: 'XXXXXXXXXXX',  // github app's Client ID
-  // GITHUB_SECRET: 'XXXXXXXXXXXXXXXXX',  // github app's Client Secret
-  GITHUB_ID: '7514ba807d37e136277d',  // github app's Client ID
-  GITHUB_SECRET: 'f30320bae6cf54cee7be7cb77f06f7ddf767a66e' //github app's Client Secret
+  GITHUB_ID: 'XXXXXXXXXXX',  // github app's Client ID
+  GITHUB_SECRET: 'XXXXXXXXXXXXXXXXX'  // github app's Client Secret
 }
