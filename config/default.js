3907fa759f2a93a8b98fa306372c26e20418ce57
YOLO <yanglu@thejoyrun.com>
2018-03-21 20:34:00 +0800

    update config

---
+++ config/default.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: 'dev',
+  port: 3000,
+  database: {
+    HOST: 'localhost',
+    USERNAME: 'root',
+    PASSWORD: '123456',
+    DATABASE: 'blog',
+    PORT: '3306'
+  },
+  GITHUB_ID: 'XXXXXXXXXXX',  // github app's Client ID
+  GITHUB_SECRET: 'XXXXXXXXXXXXXXXXX'  // github app's Client Secret
+}
